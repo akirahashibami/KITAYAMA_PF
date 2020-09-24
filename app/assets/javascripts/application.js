79e7c2c7a62c47e642b20c09761c72b6c7aff857
akirahashibami <hashibami2207@gmail.com>
2020-09-24 07:21:09 +0000

    [update] like-anime

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -24,10 +24,10 @@
 $(document).on('turbolinks:load',function(){
 
   // テキストエリアの欄を記入されたテキストに合わせて広げる
-  var $textarea = $('#textarea');
-  var lineHeight = parseInt($textarea.css('lineHeight'));
+  let $textarea = $('#textarea');
+  let lineHeight = parseInt($textarea.css('lineHeight'));
   $textarea.on('input', function(e){
-    var lines = ($(this).val() + '\n').match(/\n/g).length;
+    let lines = ($(this).val() + '\n').match(/\n/g).length;
     $(this).height(lineHeight * lines);
   });
 
